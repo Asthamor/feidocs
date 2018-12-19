9ad8f3733377136b0f43cf7d6dad5a908ef0f7b1
Maribel Tello <zS15011664@estudiantes.uv.mx>
2018-12-18 19:05:42 -0600

    cF

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,10 +1,5 @@
 require 'active_storage'
 
 class Ckeditor::Asset < ActiveRecord::Base
-  include Ckeditor::Orm::ActiveRecord::AssetBase
-  include Ckeditor::ActiveStorage
 
-  attr_accessor :data
-
-  has_one_attached :storage_data
 end
