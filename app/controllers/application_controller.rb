0fec529259f325a1a462ab719a6f72d1713ccfe4
Maribel Tello <zS15011664@estudiantes.uv.mx>
2018-12-19 12:24:57 -0600

    Se quitó foto del registro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:fullName, :personalNumber, :email, :password, :remember_me, :photo])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:fullName, :personalNumber, :email, :password, :remember_me])
         devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :remember_me])
         devise_parameter_sanitizer.permit(:account_update, keys: [:fullName, :personalNumber, :photo, :email, :password, :remember_me])
     end
