bee1e11c62625ed0b078357a7f253a22760fb31e
Maribel Tello <zS15011664@estudiantes.uv.mx>
2018-12-19 12:29:08 -0600

    Validación nombre

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -12,7 +12,8 @@ class Professor < ApplicationRecord
   has_one :signature
 
   validates :fullName, presence: { message: ": El nombre completo del profesor es obligatorio" }, length: { minimum: 2, maximum: 255,
-                                                 too_long: ": El máximo de caracteres permitidos es 255", too_short: ": El mínimo de caracteres permitidos es 2" }
+                                                 too_long: ": El máximo de caracteres permitidos es 255", too_short: ": El mínimo de caracteres permitidos es 2" },  format: { with: /\A[a-zA-Z]+\z/,
+                                                                                                                                                                               message: ": Solo se permite letras" }
   validates :personalNumber, presence: { message: ": El número de personal es obligatorio" }, uniqueness: { message: ": El número de personal debe ser único" }, numericality: { only_integer: true , message: ": El número de personal debe ser conformado por números enteros" } , length: { maximum: 255,
                                                                                               too_long: ": El máximo de caracteres permitidos es %{count}" }
 
