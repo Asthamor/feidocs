656d04853e4bf94c14a53b3298aca4b4557ed0fb
Maribel Tello <zS15011664@estudiantes.uv.mx>
2018-12-18 18:48:31 -0600

    cambios

--- db/migrate/20181212045507_add_confirmable.rb
+++ db/migrate/20181212045507_add_confirmable.rb
@@ -1,8 +1,8 @@
 class AddConfirmable < ActiveRecord::Migration[5.2]
   def up
-    add_column :professors, :confirmation_token, :string
-    add_column :professors, :confirmed_at, :datetime
-    add_column :professors, :confirmation_sent_at, :datetime
+   # add_column :professors, :confirmation_token, :string
+   # add_column :professors, :confirmed_at, :datetime
+   # add_column :professors, :confirmation_sent_at, :datetime
     # add_column :professors, :unconfirmed_email, :string # Only if using reconfirmable
     add_index :professors, :confirmation_token, unique: true
     # User.reset_column_information # Need for some types of updates, but not for update_all.
